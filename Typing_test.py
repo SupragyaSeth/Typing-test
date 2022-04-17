73161ed32e286ccbab7775f8c03fbdcb423754e7
SupragyaSeth <94500313+SupragyaSeth@users.noreply.github.com>
2022-04-17 00:04:44 -0700

    Added pygame elements

--- Typing_test.py
+++ Typing_test.py
@@ -1,19 +1,44 @@
 from random import random
 import time
 import random
+import pygame
+
+WIDTH, HEIGHT = 1000, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 randlist = ["come", "when", "it", "logo"]
 random.shuffle(randlist)
 wordcount = len(randlist)
 print(*randlist)
 
+#Starts pygame
+def main():
+    
+    run = True
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
+
+#Calculations
 while True:
     t1 = time.time()
     textinput = str(input("Enter the Sentence:"))
     t2 = time.time()
     accuracy = len(set(textinput.split())&set(randlist))
-    accuracy = accuracy/wordcount
+    accuracy = (accuracy/wordcount) * 100
     timetaken = t2 - t1
-    wpm = wordcount / timetaken
-    print("WPM", wpm, "Accuracy", accuracy, "Timetaken", timetaken)
+    wpm = (wordcount * 60) / timetaken
+
+    wpm = round(wpm)
+    accuracy = round(accuracy)
+    timetaken = round(timetaken)
+
+    print("WPM", wpm, "Accuracy", accuracy , "Timetaken", timetaken)
     break
--- type test test.py
+++ type test test.py
@@ -1,17 +1,4 @@
-import time
+passage1 = "Exactly what I myself had thought, many times. Exactly what was perfectly manifest to me at the moment. But how could I, a poor dazed village lad, avoid that wonderful inconsistency into which the best and wisest of men fall every day?"
 
+passage2 = "After a while they played at forfeits; for it is good to be children sometimes, and never better than at Christmas, when its mighty Founder was a child himself. There was first a game at blind-man's buff. Of course there was."
 
-string = "hello this is bob"
-wordcount = len(string.split())
-print(string)
-
-while True:
-    t1 = time.time()
-    textinput = str(input("Enter the Sentence:"))
-    t2 = time.time()
-    accuracy = len(set(textinput.split())&set(string.split()))
-    accuracy = accuracy/wordcount
-    timetaken = t2 - t1
-    wpm = wordcount / timetaken
-    print("WPM", wpm, "Accuracy", accuracy, "Timetaken", timetaken)
-    break
