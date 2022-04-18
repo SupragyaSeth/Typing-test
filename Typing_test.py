fe3b6e00777c83aac793a91508d57b92fd2a4d57
SupragyaSeth <94500313+SupragyaSeth@users.noreply.github.com>
2022-04-17 23:05:18 -0700

    Tried adding classes

--- Typing_test.py
+++ Typing_test.py
@@ -2,43 +2,49 @@ from random import random
 import time
 import random
 import pygame
+import sys
 
-WIDTH, HEIGHT = 1000, 800
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+class Game:
 
-randlist = ["come", "when", "it", "logo"]
-random.shuffle(randlist)
-wordcount = len(randlist)
-print(*randlist)
+    WIDTH, HEIGHT = 1000, 800
+    WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Typing Test")
 
-#Starts pygame
-def main():
+    randlist = ["come", "when", "it", "logo"]
+    random.shuffle(randlist)
+    wordcount = len(randlist)
+    print(*randlist)
+
+    #Starts pygame
+
+    def main():
+        
+        run = True
+        while run:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+        
+        pygame.quit()
+
+    if __name__ == "__main__":
+        main()
+
+    #Calculations
     
-    run = True
-    while run:
+    while True:
+        t1 = time.time()
+        textinput = str(input("Enter the Sentence:"))
+        t2 = time.time()
+        accuracy = len(set(textinput.split())&set(randlist))
+        accuracy = (accuracy/wordcount) * 100
+        timetaken = t2 - t1
+        wpm = (wordcount * 60) / timetaken
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-    
-    pygame.quit()
+        wpm = round(wpm)
+        accuracy = round(accuracy)
+        timetaken = round(timetaken)
 
-if __name__ == "__main__":
-    main()
-
-#Calculations
-while True:
-    t1 = time.time()
-    textinput = str(input("Enter the Sentence:"))
-    t2 = time.time()
-    accuracy = len(set(textinput.split())&set(randlist))
-    accuracy = (accuracy/wordcount) * 100
-    timetaken = t2 - t1
-    wpm = (wordcount * 60) / timetaken
-
-    wpm = round(wpm)
-    accuracy = round(accuracy)
-    timetaken = round(timetaken)
-
-    print("WPM", wpm, "Accuracy", accuracy , "Timetaken", timetaken)
-    break
+        print("WPM", wpm, "Accuracy", accuracy , "Timetaken", timetaken)
+        break
