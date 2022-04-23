435894569e2c46b63e9926818408135f671004e0
SupragyaSeth <94500313+SupragyaSeth@users.noreply.github.com>
2022-04-22 17:59:55 -0700

    Renamed file

--- Typing_Tester.py
+++ Typing_Tester.py
@@ -14,6 +14,21 @@ def FindAccuracy(input_passage,output_passage) :
         count=count+1
     print(word_match)    
 
+#Functions
+def Accuracy(l_textinput, l_originalstr):
+    split_input = l_textinput.split()
+    wordcount = len(l_originalstr)
+    accuracy = len(set(l_textinput.split())&set(l_originalstr))
+    accuracy = (accuracy/wordcount) * 100 
+    accuracy = round(accuracy)
+    return accuracy
+
+def WPM(l_textinput, l_timetaken):
+    list_wordcount = l_textinput.split()
+    temp_wordcount = len(list_wordcount)
+    wpm = (temp_wordcount * 60) / l_timetaken
+    wpm = round(wpm)
+    return wpm
 
 #Passages that the user has to type
 passage1 = "The cat walked down the road"
@@ -24,29 +39,14 @@ passage4 = "The mouse ate the cheese"
 randlist = [passage1, passage2, passage3, passage4]
 choice = random.choice(randlist)
 value = choice.split()
-wordcount = len(value)
-print( "\n" + choice)
+print( "\n" + choice) 
 
-#Calculations
+#Finding time
 input("\n Press Enter to Start...")
 t1 = time.time()
 textinput = str(input("\n" + "Enter the Sentence: "))
-
 t2 = time.time()
-split_input = textinput.split()
-FindAccuracy(value,split_input)
-accuracy = len(set(textinput.split())&set(value))
-accuracy = (accuracy/wordcount) * 100
-
 timetaken = t2 - t1
-
-textval = textinput.split()
-textval2 = len(textval)
-
-wpm = (textval2 * 60) / timetaken
-wpm = round(wpm)
-
-accuracy = round(accuracy)
 timetaken = round(timetaken)
 
-print("\n" + "WPM", wpm, "Accuracy", accuracy , "Timetaken", timetaken)
+print("\n" + "WPM:", WPM(textinput, timetaken), "Accuracy:", Accuracy(textinput, value) , "Timetaken:", timetaken)
